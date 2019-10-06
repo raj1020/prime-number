e12b7348ef4e23af31e28785da38f11e465c7e59
raj1020 <raj.1020@hotmail.com>
2019-10-06 23:01:53 +1100

    completed a functioning program

--- primenumber.rb
+++ primenumber.rb
@@ -5,10 +5,17 @@ def is_it_prime(num)
     end
     if num==2
         return "This is a prime number"
-    
+    else
 
         for i in (2..num)
-            if num%i
+            if num%i == 0
+                return "This is not a prime number"
+                break
+            end
+            if i>(num/2)
+                return "This is a prime number"
+                break
+            end
         end
     end
 
@@ -16,6 +23,6 @@ def is_it_prime(num)
 end
 
 
-is_it_prime(7)
-is_it_prime(100)
+p is_it_prime(7)
+p is_it_prime(100)
 
