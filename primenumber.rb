e06d9c6013dd7092adac1c547857237f19844097
raj1020 <raj.1020@hotmail.com>
2019-10-06 23:09:18 +1100

    added question at the top as comments

--- primenumber.rb
+++ primenumber.rb
@@ -1,3 +1,14 @@
+
+#Create a method that takes a number as an argument and returns "This is a prime number", 
+#if its a prime number, otherwise it returns "This is not a prime number". Note, 
+#negative numbers can not be considered prime. 0 and 1 are also not prime!
+
+#E.g. is_it_prime(7), should return "This is a prime number"
+
+#E.g. is_it_prime(100), should return "This is not a prime number"
+
+
+
 def is_it_prime(num)
     #Your code here
     if num<2
