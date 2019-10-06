2beb5751699b509c4d75c434d7f45a9d8244596b
raj1020 <raj.1020@hotmail.com>
2019-10-06 22:49:27 +1100

    initial method definition for prime number

---
+++ primenumber.rb
@@ -0,0 +1,21 @@
+def is_it_prime(num)
+    #Your code here
+    if num<2
+        return "Please provide an integer that is equal to or greater than 2."
+    end
+    if num==2
+        return "This is a prime number"
+    
+
+        for i in (2..num)
+            if num%i
+        end
+    end
+
+
+end
+
+
+is_it_prime(7)
+is_it_prime(100)
+
